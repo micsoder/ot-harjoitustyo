ca016b93512786e029a8dce45f897c36271d2f78
micsoder <michaela.x.soderholm@helsinki.fi>
2023-12-10 12:59:09 +0200

    changed the pictures

--- src/index.py
+++ src/index.py
@@ -2,6 +2,14 @@ from main import Main
 
 
 def index():
+    """Entry point for the application.
+
+    This function initializes the main functionality of the program by creating
+    an instance of the `Main` class and invoking its constructor.
+
+    Returns:
+        None
+    """
 
     Main()
 
