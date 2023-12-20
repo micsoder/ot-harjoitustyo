1fdd3e0bd9f13957be7e9fa4dd9abea61191d1fa
micsoder <michaela.x.soderholm@helsinki.fi>
2023-12-20 08:17:14 +0200

    added colors to baseframe

--- src/ui/base_frame.py
+++ src/ui/base_frame.py
@@ -17,7 +17,7 @@ class BaseFrame():
 
     def __init__(self):
         """Initializes the BaseFrame with predefined fonts."""
-        self.hand = 'hand2'
+        self.hand2 = 'hand2'
 
         self.font1 = ('Helvetica', 25, 'bold')
         self.font2 = ('Arial', 17, 'bold')
